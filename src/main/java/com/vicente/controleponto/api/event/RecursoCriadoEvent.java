4a71e69f0c08f754b94bb22bf6fead8a552ca064
Vicente Silva <vicente.pspenha@gmail.com>
2020-05-17 00:45:26 -0300

    Criado classes para escutar eventos.

---
+++ src/main/java/com/vicente/controleponto/api/event/RecursoCriadoEvent.java
@@ -0,0 +1,25 @@
+package com.vicente.controleponto.api.event;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationEvent;
+
+import lombok.Getter;
+
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Getter
+	private HttpServletResponse response;
+	
+	@Getter
+	private Long id;
+	
+	public RecursoCriadoEvent(Object source, HttpServletResponse response, Long id) {
+		super(source);
+		this.response = response;
+		this.id = id;
+	}
+
+}
