45cee805cd7078dab1544b49657a07c12ca704da
Vicente Silva <vicente.pspenha@gmail.com>
2020-05-21 13:59:06 -0300

    Script para criar tabela e sequence para recurso Registro.

---
+++ src/main/resources/db/migration/V004__criar_tabela_registro.sql
@@ -0,0 +1,15 @@
+/*
+script para criar tabela registro
+*/
+CREATE TABLE IF NOT EXISTS registro (
+	id INTEGER NOT NULL,
+	data DATE NOT NULL DEFAULT CURRENT_DATE,
+	usuario_id INTEGER NOT NULL,
+	CONSTRAINT pk_registro PRIMARY KEY (id),	
+	CONSTRAINT fk_registro_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
+
+/*
+script para criar sequence registro_seq
+*/
+CREATE SEQUENCE IF NOT EXISTS registro_seq START 1;
