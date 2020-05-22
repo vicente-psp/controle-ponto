31814d066fe2b0082f6b67643b1eaee10ba13823
Vicente Silva <vicente.pspenha@gmail.com>
2020-05-22 13:55:38 -0300

    Script para criar tabela entrada_saida.

---
+++ src/main/resources/db/migration/V005__criar_tabela_entrada_saida.sql
@@ -0,0 +1,15 @@
+/*
+script para criar tabela entrada_saida
+*/
+CREATE TABLE IF NOT EXISTS entrada_saida (
+	id INTEGER NOT NULL,
+	registro_id INTEGER NOT NULL,
+	entrada TIME NOT NULL DEFAULT CURRENT_TIME,
+	saida TIME,
+	CONSTRAINT fk_entrada_saida_registro FOREIGN KEY (registro_id) REFERENCES registro(id)
+);
+
+/*
+script para criar sequence entrada_saida_seq
+*/
+CREATE SEQUENCE IF NOT EXISTS entrada_saida_seq START 1;
