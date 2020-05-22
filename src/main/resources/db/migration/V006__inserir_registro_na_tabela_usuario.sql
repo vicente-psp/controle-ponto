930ec2723073abcb656667e37dda51ea9d76f378
Vicente Silva <vicente.pspenha@gmail.com>
2020-05-22 13:56:20 -0300

    Script para inserir usuário Admin na tabela usuario.

---
+++ src/main/resources/db/migration/V006__inserir_registro_na_tabela_usuario.sql
@@ -0,0 +1,6 @@
+/*
+script para inserir primeiro registro na tabela usuário
+*/
+INSERT INTO
+	usuario (id, nome, email, senha, tipo_usuario)
+	VALUES(1, 'Admin', 'adm@controleponto.com.br', '$2a$10$6VxWhdkIljrJaDzY1Ck1ZO7RbUkJJflFgw0j3as3M8k8k9YqIdlYy', 'ADM');
