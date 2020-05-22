985da3fbc286257c0fa597f4217cf48fbb3be0f7
Vicente Silva <vicente.pspenha@gmail.com>
2020-05-22 13:50:21 -0300

    Criado método para buscar usuário por email.

--- src/main/java/com/vicente/controleponto/api/repositories/UsuarioRepository.java
+++ src/main/java/com/vicente/controleponto/api/repositories/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package com.vicente.controleponto.api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vicente.controleponto.api.models.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+	public Optional<Usuario> findByEmail(String email);
+	
 }
