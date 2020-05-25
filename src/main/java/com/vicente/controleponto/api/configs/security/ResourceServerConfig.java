a301704831a62f252438f157b40c36bd4371b314
Vicente Silva <vicente.pspenha@gmail.com>
2020-05-25 00:06:00 -0300

    Liberado rotas para usuário solicitar e confirmar cadastro.

--- src/main/java/com/vicente/controleponto/api/configs/security/ResourceServerConfig.java
+++ src/main/java/com/vicente/controleponto/api/configs/security/ResourceServerConfig.java
@@ -23,6 +23,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+					.antMatchers("/usuarios/solicitar-cadastro").permitAll()
+					.antMatchers("/usuarios/confirmar-cadastro").permitAll()
 					.anyRequest().authenticated()
 					.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 					.and().csrf().disable();
