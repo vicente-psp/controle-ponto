7448dc435a3c1895a6570369b74a8b843a5bd72b
Vicente Silva <vicente.pspenha@gmail.com>
2020-06-05 00:38:33 -0300

    Correção na assinatura do método que lista entrada e saídas pelo registro.

--- src/main/java/com/vicente/controleponto/api/repositories/EntradaSaidaRepository.java
+++ src/main/java/com/vicente/controleponto/api/repositories/EntradaSaidaRepository.java
@@ -1,9 +1,14 @@
 package com.vicente.controleponto.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vicente.controleponto.api.models.EntradaSaida;
+import com.vicente.controleponto.api.models.Registro;
 
 public interface EntradaSaidaRepository extends JpaRepository<EntradaSaida, Long> {
+	
+	public List<EntradaSaida> findByRegistro(Registro registro);
 
 }
