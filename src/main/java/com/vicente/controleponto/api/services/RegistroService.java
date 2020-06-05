d6af341f8a6741f98291518fe911edadee73cd36
Vicente Silva <vicente.pspenha@gmail.com>
2020-06-05 01:16:26 -0300

    Update RegistroService.java

--- src/main/java/com/vicente/controleponto/api/services/RegistroService.java
+++ src/main/java/com/vicente/controleponto/api/services/RegistroService.java
@@ -1,10 +1,12 @@
 package com.vicente.controleponto.api.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,19 @@ import com.vicente.controleponto.api.repositories.RegistroRepository;
 public class RegistroService implements GenericsOperationsService<Registro> {
 	
 	@Autowired RegistroRepository repository;
+	
+	public boolean isRegistroInData(LocalDate localDate) {
+		Optional<Registro> optional = repository.findByData(localDate);
+		return optional.isPresent();
+	}
+	
+	public Registro findByData(LocalDate localDate) {
+		Optional<Registro> optional = repository.findByData(localDate);
+		if (!optional.isPresent()) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		return optional.get();
+	}
 	
 	public List<Registro> findAll() {
 		return repository.findAll();
@@ -32,6 +47,9 @@ public class RegistroService implements GenericsOperationsService<Registro> {
 
 	@Override
 	public Registro insert(Registro entity) {
+		if (isRegistroInData(entity.getData())) {
+			throw new DataIntegrityViolationException("Operação não permitida, já existe um registro nesta data.");
+		}
 		return repository.save(entity);
 	}
 
