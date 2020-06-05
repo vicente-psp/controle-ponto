8d5b1535ef67b0b9bea11e2948fa92802bdd97e4
Vicente Silva <vicente.pspenha@gmail.com>
2020-06-05 01:16:30 -0300

    Update EntradaSaidaService.java

--- src/main/java/com/vicente/controleponto/api/services/EntradaSaidaService.java
+++ src/main/java/com/vicente/controleponto/api/services/EntradaSaidaService.java
@@ -1,5 +1,8 @@
 package com.vicente.controleponto.api.services;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import com.vicente.controleponto.api.generics.GenericsOperationsService;
 import com.vicente.controleponto.api.models.EntradaSaida;
+import com.vicente.controleponto.api.models.Registro;
 import com.vicente.controleponto.api.repositories.EntradaSaidaRepository;
 
 @Service
@@ -17,6 +21,41 @@ public class EntradaSaidaService implements GenericsOperationsService<EntradaSai
 	
 	@Autowired EntradaSaidaRepository repository;
 	@Autowired RegistroService registroService;
+	
+	/*
+	public void registrarEntradaSaida(Date time) {
+		LocalTime t = LocalTime.now();
+		System.out.println("t => " + t);
+		LocalDate localDate = LocalDate.now();
+		if (registroService.isRegistroInData(localDate)) {
+			Registro registro = registroService.findByData(localDate);
+			
+			if (!isEntradaSaidaInRegistro(registro)) {
+				EntradaSaida entradaSaida = EntradaSaida.builder().entrada(time).build();
+				repository.save(entradaSaida);
+			} else {
+				List<EntradaSaida> entradaSaidas = listByRegistro(registro);
+				EntradaSaida entradaSaida = new EntradaSaida();
+				entradaSaidas.forEach(item -> {
+					if (item.getSaida() == null) {
+						entradaSaida = item;
+					}
+				});
+			}
+			for (int i = 0; i < registro.getEntradaSaidas().size(); i++) {
+				
+			}
+		}
+	}
+	*/
+	
+	private List<EntradaSaida> listByRegistro(Registro registro) {
+		return repository.findByRegistro(registro);
+	}
+	
+	private boolean isEntradaSaidaInRegistro(Registro registro) {
+		return registro.getEntradaSaidas() == null || registro.getEntradaSaidas().size() == 0;
+	}
 	
 	public List<EntradaSaida> findAll() {
 		return repository.findAll();
