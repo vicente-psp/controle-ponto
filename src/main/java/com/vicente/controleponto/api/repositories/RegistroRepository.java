727d017d0b2273ec99b9e3a22dfd2581026e2717
Vicente Silva <vicente.pspenha@gmail.com>
2020-06-05 00:51:33 -0300

    Criado método para buscar Registro pela data.

--- src/main/java/com/vicente/controleponto/api/repositories/RegistroRepository.java
+++ src/main/java/com/vicente/controleponto/api/repositories/RegistroRepository.java
@@ -1,9 +1,14 @@
 package com.vicente.controleponto.api.repositories;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vicente.controleponto.api.models.Registro;
 
 public interface RegistroRepository extends JpaRepository<Registro, Long> {
 
+	public Optional<Registro> findByData(LocalDate data);
+	
 }
