41312bbb67344778afbbbc393a05ccd8b99bfc6c
Vicente Silva <vicente.pspenha@gmail.com>
2021-01-10 06:43:34 -0300

    Modificado scripts.

--- src/main/resources/db/migration/V001__criar_tabela_usuario.sql
+++ src/main/resources/db/migration/V001__criar_tabela_usuario.sql
@@ -1,29 +1,23 @@
-/*
-script para criar tabela usuario
-*/
+/* script para criar sequence usuario_seq */
+CREATE SEQUENCE IF NOT EXISTS usuario_seq START 1;
+
+
+/* script para criar tabela usuario */
 CREATE TABLE IF NOT EXISTS usuario (
-	id INTEGER NOT NULL,
-	nome VARCHAR(150) NOT NULL,
+	id INTEGER NOT NULL DEFAULT nextval('usuario_seq'),
 	email VARCHAR(150) NOT NULL,
 	senha VARCHAR(200) NOT NULL,
 	tipo_usuario CHARACTER(3) NOT NULL,
 	data_inclusao TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	data_alteracao TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	data_alteracao TIMESTAMP WITH TIME ZONE,
 	ativo BOOLEAN NOT NULL DEFAULT FALSE,
+	cadastro_aprovado BOOLEAN NOT NULL DEFAULT FALSE,
 	CONSTRAINT u_usuario_email UNIQUE(email),
 	CONSTRAINT pk_usuario PRIMARY KEY (id)
 );
 
 
-/*
-script para criar sequence usuario_seq
-*/
-CREATE SEQUENCE IF NOT EXISTS usuario_seq START 2;
-
-
-/*
-script para inserir primeiro registro na tabela usuário
-*/
+/* script para inserir primeiro registro na tabela usuário */
 INSERT INTO
-	usuario (id, nome, email, senha, tipo_usuario, ativo)
-	VALUES(1, 'Admin', 'adm@controleponto.com.br', '$2a$10$6VxWhdkIljrJaDzY1Ck1ZO7RbUkJJflFgw0j3as3M8k8k9YqIdlYy', 'ADM', true);
+	usuario (email, senha, tipo_usuario, ativo)
+	VALUES('adm@controleponto.com.br', '$2a$10$ySylNvwY6oRvlenA2GhmiuewW8dl5svOnvRWpbUCW5hu8pvduNELy', 'ADM', true);
--- src/main/resources/db/migration/V006__criar_tabela_solicitacao_cadastro.sql
+++ src/main/resources/db/migration/V006__criar_tabela_solicitacao_cadastro.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS solicitacao_cadastro (
 	id INTEGER NOT NULL,
 	usuario_id INTEGER NOT NULL,
 	secret_key CHARACTER(50) NOT NULL,
+	data_solicitacao TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	CONSTRAINT u_solicitacao_cadastro_usuario UNIQUE (usuario_id),
 	CONSTRAINT fk_solicitacao_cadastro_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id),
 	CONSTRAINT pk_solicitacao_cadastro PRIMARY KEY (id)
