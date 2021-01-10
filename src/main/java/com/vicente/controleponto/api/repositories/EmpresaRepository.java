b81aad7ab8db4a6dc8692f6d5f1e89eb14d25f2f
Vicente Silva <vicente.pspenha@gmail.com>
2021-01-10 06:40:38 -0300

    Removido recursos não utilizados.

--- src/main/java/com/vicente/controleponto/api/repositories/EmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vicente.controleponto.api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vicente.controleponto.api.models.Empresa;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-
-}
