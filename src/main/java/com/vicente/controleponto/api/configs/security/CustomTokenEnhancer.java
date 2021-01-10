44b8b6a88d57753f07c680a713c349f828c10a0d
Vicente Silva <vicente.pspenha@gmail.com>
2021-01-10 06:45:33 -0300

    Update CustomTokenEnhancer.java

--- src/main/java/com/vicente/controleponto/api/configs/security/CustomTokenEnhancer.java
+++ src/main/java/com/vicente/controleponto/api/configs/security/CustomTokenEnhancer.java
@@ -16,7 +16,7 @@ public class CustomTokenEnhancer implements TokenEnhancer {
 		UsuarioSistema usuarioSistema = (UsuarioSistema) authentication.getPrincipal();
 		
 		Map<String, Object> addInfo = new HashMap<>();
-		addInfo.put("nome", usuarioSistema.getUsuario().getNome());
+//		addInfo.put("nome", usuarioSistema.getUsuario().getNome());
 		
 		((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(addInfo);
 		return accessToken;
